c686c6beef0bad37f7b66dc057494d2f4822e574
Michiel <2036886+acidzebra@users.noreply.github.com>
2018-04-02 21:31:54 +0200

    minor changes and tweaks

--- event_monitor.py
+++ event_monitor.py
@@ -58,7 +58,12 @@ class CheckState (threading.Thread):
 					robot.wait_for_all_actions_completed()
 					#robot.play_anim_trigger(cozmo.anim.Triggers.TurtleRoll, ignore_body_track=True).wait_for_completed()
 					robot.play_anim_trigger(cozmo.anim.Triggers.AskToBeRightedLeft, ignore_body_track=False).wait_for_completed()
-					robot.play_anim_trigger(cozmo.anim.Triggers.TurtleRoll, ignore_body_track=False).wait_for_completed()
+					time.sleep(0.5)
+					if robot.is_cliff_detected:
+						robot.play_anim_trigger(cozmo.anim.Triggers.TurtleRoll, ignore_body_track=False).wait_for_completed()
+					time.sleep(0.5)
+					if robot.is_cliff_detected:
+						robot.drive_wheels(-40, -40, l_wheel_acc=30, r_wheel_acc=30, duration=1.0)
 					#robot.play_anim_trigger(cozmo.anim.Triggers.FlipDownFromBack, ignore_body_track=True).wait_for_completed()
 					#robot.play_anim_trigger(cozmo.anim.Triggers.CodeLabUnhappy, ignore_body_track=True).wait_for_completed()
 					is_picked_up = True
